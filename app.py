c6b5f62b3bbb14314dbc90bca0d94bb2eff00adf
2023mt03079 <2023mt03079@wilp.bits-pilani.ac.in>
2025-03-22 18:13:13 +0530

    added task definition for ECS

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain_community.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_community.chat_models import ChatOpenAI
-from langchain_community.memory import ConversationBufferMemory
-from langchain_community.chains import ConversationalRetrievalChain
+# from langchain_community.text_splitter import CharacterTextSplitter
+# from langchain_community.embeddings import OpenAIEmbeddings
+# from langchain_community.vectorstores import FAISS
+# from langchain_community.chat_models import ChatOpenAI
+# from langchain_community.memory import ConversationBufferMemory
+# from langchain_community.chains import ConversationalRetrievalChain
+# from htmlTemplates import css, bot_template, user_template
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 
 
